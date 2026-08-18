641d558560273c765042ff5d76016be391b1fd5b
Sahisahithya19 <nallamillisahithyasrireddy@gmail.com>
2026-08-18 11:12:51 +0530

    Create BasePage.java

---
+++ BasePage.java
@@ -0,0 +1,65 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+
+        this.driver = driver;
+
+        wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(10)
+        );
+    }
+
+    protected void click(By locator) {
+
+        WebElement element =
+                wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+        element.click();
+    }
+
+    protected void enterText(By locator, String text) {
+
+        WebElement element =
+                wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+
+        WebElement element =
+                wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        return element.getText();
+    }
+
+    protected boolean isDisplayed(By locator) {
+
+        try {
+
+            WebElement element =
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+            return element.isDisplayed();
+
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
+}
