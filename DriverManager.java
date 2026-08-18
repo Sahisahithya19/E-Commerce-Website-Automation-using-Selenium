17832412407e339b2917ba6d1496c4b41d057a6b
Sahisahithya19 <nallamillisahithyasrireddy@gmail.com>
2026-08-18 11:12:25 +0530

    Create DriverManager.java

---
+++ DriverManager.java
@@ -0,0 +1,34 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DriverManager {
+
+    private static WebDriver driver;
+
+    public static void initializeDriver() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void quitDriver() {
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+
+    }
+}
